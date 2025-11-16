fb9377c147b6feaacdd7a3bd46b9c3abf8480886
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-16 22:13:55 +0100

    stuff that doesnt work

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,8 +20,8 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version("2.0.20")
-        id("dev.deftu.gradle.multiversion-root") version("2.51.0")
+        kotlin("jvm") version("2.2.10")
+        id("dev.deftu.gradle.multiversion-root") version("2.62.0")
     }
 }
 
